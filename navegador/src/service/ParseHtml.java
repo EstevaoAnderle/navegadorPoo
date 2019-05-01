707572faeed9be2eea63adc65371376d2ae0c1ed
Lenon Szynwwelski de Paula <lenonsdp@gmail.com.br>
2019-05-01 00:08:31 -0300

    Revisões, criação das classes no e parseHtml;

---
+++ navegador/src/service/ParseHtml.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+/**
+ *
+ * @author lenon.060194
+ */
+public class ParseHtml {
+       public void parseArvore (String parse){
+        Nos n = new Nos();
+        System.out.println(parse);
+        
+    }
+}
